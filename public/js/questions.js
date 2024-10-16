ed6274e74c15e9f83ce5baec6b2b368039a992cc
LAPTOP-QMITUAI4\hluca <lucas.hureaux@etudiant.univ-reims.fr>
2024-10-16 16:00:20 +0200

    Add events on click of answer buttons + detection on if the right answer is clicked or not

--- public/js/questions.js
+++ public/js/questions.js
@@ -1,14 +1,28 @@
+buttons = [document.getElementById("answer1"), 
+    document.getElementById("answer2"), 
+    document.getElementById("answer3"), 
+    document.getElementById("answer4")]
 
 fetch("http://localhost:8000/src/questionGenerator.php").then(
     response => response.json()
 ).then(
     question => {
-        console.log(question.question.question)
+        console.log(question.question.correctAnswer)
         document.getElementById("question").innerHTML = question.question.question
-        document.getElementById("answer1").innerHTML = question.question.answers[0]
-        document.getElementById("answer2").innerHTML = question.question.answers[1]
-        document.getElementById("answer3").innerHTML = question.question.answers[2]
-        document.getElementById("answer4").innerHTML = question.question.answers[3]
+        buttons[0].innerHTML = question.question.answers[0]
+        buttons[1].innerHTML = question.question.answers[1]
+        buttons[2].innerHTML = question.question.answers[2]
+        buttons[3].innerHTML = question.question.answers[3]
+        buttons.forEach((button, index) => {
+            button.addEventListener("click", function() {
+                console.log(index, question.question.correctAnswer)
+                if (index === question.question.correctAnswer) {
+                    document.getElementById("message").innerHTML = "Bonne réponse! :)"
+                } else {
+                    document.getElementById("message").innerHTML = "Mauvaise réponse! :("
+                }
+            })
+        })
     }
 ).catch(
     error => console.error("Erreur :", error)
