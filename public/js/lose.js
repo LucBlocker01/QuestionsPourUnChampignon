964eb3829fd3024898d9cf8299d0e9dd468dcf24
LAPTOP-QMITUAI4\hluca <lucas.hureaux@etudiant.univ-reims.fr>
2024-10-28 09:30:36 +0100

    if null/20 in lose screen, redirect to index screen

--- public/js/lose.js
+++ public/js/lose.js
@@ -1,3 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    if (!localStorage.getItem("currentQuestion")) {
+        location.replace("index.html");
+    }
+})
+
+
+
 document.getElementById("messageText").innerHTML = 
 "Vous êtes tombé à court de vies. Vous avez répondu à "
 +localStorage.getItem("currentQuestion")
